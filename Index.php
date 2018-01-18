9fcba0100ffe7e6a2be469fb9a4969937afb9750
zakiyamin96 <35558813+zakiyamin96@users.noreply.github.com>
2018-01-18 15:22:16 +0700

    Add files via upload

---
+++ Index.php
@@ -0,0 +1,26 @@
+<div id="wb_Form1" style="position:absolute;width:431px;height:403px;">
+<form name="Upload MOD Minecraft" method="post" action="<?php echo basename(__FILE__); ?>" enctype="multipart/form-data" id="Form1">
+<div id="wb_Text1" style="position:absolute;left:13px;top:17px;width:79px;height:16px;z-index:30;text-align:left;">
+<span style="color:#000000;font-family:Arial;font-size:13px;">Nama MOD:</span></div>
+<input type="text" id="Editbox1" style="position:absolute;left:97px;top:19px;width:198px;height:23px;line-height:23px;z-index:31;" name="Nama MOD " value="" required="required" placeholder="Nama MOD Ini">
+<div id="wb_Text2" style="position:absolute;left:16px;top:53px;width:62px;height:16px;z-index:32;text-align:left;">
+<span style="color:#000000;font-family:Arial;font-size:13px;">Uploader:</span></div>
+<input type="text" id="Editbox2" style="position:absolute;left:97px;top:49px;width:198px;height:23px;line-height:23px;z-index:33;" name="Uploader " value="" placeholder="Namamu">
+<input type="submit" id="Button1" name="" value="Upload MOD" style="position:absolute;left:69px;top:350px;width:96px;height:25px;z-index:34;">
+<div id="wb_Text3" style="position:absolute;left:11px;top:93px;width:97px;height:32px;z-index:35;text-align:left;">
+<span style="color:#000000;font-family:Arial;font-size:13px;">Sumber/Author :</span></div>
+<input type="text" id="Editbox3" style="position:absolute;left:112px;top:89px;width:198px;height:23px;line-height:23px;z-index:36;" name="Sumber/Author " value="" placeholder="pembuat MOD ini">
+<div id="wb_Text4" style="position:absolute;left:10px;top:127px;width:99px;height:32px;z-index:37;text-align:left;">
+<span style="color:#000000;font-family:Arial;font-size:13px;">Deskripsi MOD :</span></div>
+<textarea name="Deskripsi MOD " id="TextArea1" style="position:absolute;left:113px;top:124px;width:250px;height:98px;z-index:38;" rows="5" cols="36" required="required">Katakan sesuatu tentang mod ini,
+ misal replace apa, 
+fungsinya apa, 
+ceritanya bagaimana dan lain lain</textarea>
+<input type="file" id="FileUpload1" style="position:absolute;left:120px;top:251px;width:198px;height:21px;z-index:39;" name="File MOD " required="required">
+<div id="wb_Text5" style="position:absolute;left:39px;top:254px;width:67px;height:16px;z-index:40;text-align:left;">
+<span style="color:#000000;font-family:Arial;font-size:13px;">File MOD :</span></div>
+<input type="file" id="FileUpload2" style="position:absolute;left:120px;top:284px;width:198px;height:21px;z-index:41;" name="Gambar/Screenshot " required="required">
+<div id="wb_Text6" style="position:absolute;left:35px;top:289px;width:79px;height:16px;z-index:42;text-align:left;">
+<span style="color:#000000;font-family:Arial;font-size:13px;">Screenshot :</span></div>
+</form>
+</div>
